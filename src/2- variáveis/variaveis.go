154e607991a75b603c3924a48f16eded68dec104
EljonasCavalcante <eljonascavalcante@gmai.com>
2023-04-19 17:13:05 -0300

    feat(go):create var

--- src/2- variáveis/variaveis.go
+++ src/2- variáveis/variaveis.go
@@ -6,19 +6,16 @@ func main() {
 	var variavel1 string = "variável 1"
 	variavel2 := "variével 2"
 
-
-
 	fmt.Println(variavel1)
 	fmt.Println(variavel2)
 
-
 	var (
-		variavel3 string = "jdfiasijd"
-		variavel4 string = "jdfiasijd"
+		variavel3 string = "Primeiro teste"
+		variavel4 string = "Variavel teste"
 	)
 	fmt.Println(variavel3, variavel4)
 
-	variavel5, vavariavel6 := "xxxxx","bbbbbb"
+	variavel5, vavariavel6 := "xxxxx", "bbbbbb"
 
-	fmt.Println(variavel5,vavariavel6)
+	fmt.Println(variavel5, vavariavel6)
 }
