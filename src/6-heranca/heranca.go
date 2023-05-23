09e21cc744f21ab89e287d0c1ae893492f2cd8a1
EljonasCavalcante <eljonascavalcante@gmai.com>
2023-05-23 14:31:26 -0300

    feat(ESTD-06):update file

---
+++ src/6-heranca/heranca.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+	p1 := pessoa{"João", "Pedro", 20, 178}
+	fmt.Println(p1)
+}
