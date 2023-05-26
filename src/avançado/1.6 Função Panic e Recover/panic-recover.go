2b968274fb17739babdd38f5272efc59d97ba69b
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-05-26 18:21:31 -0300

    feat(STUD-1.6):creating function function panc and recorve

---
+++ src/avançado/1.6 Função Panic e Recover/panic-recover.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func alunaAprovado(n1, n2 float64) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+	panic("A MEDIA É EXATAMENTE 6!")
+}
+
+func main() {
+	fmt.Println(alunaAprovado(6, 6))
+	fmt.Println("Pós execução")
+}
