7cd9cda363b58b1478ff93c21d5cdc2bdeb9d238
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-05-26 18:19:25 -0300

    feat(STUD-1.4)creating function recusiva

--- src/avançado/1.4 Função Recursiva/recursiva.go
+++ src/avançado/1.4 Função Recursiva/recursiva.go
@@ -4,6 +4,26 @@ package main
 
 import "fmt"
 
+// A sucessão de Fibonacci é uma sequência de números inteiros iniciados por zero e um, no qual cada termo subsequente corresponde a soma dos dois números anteriores: 0,1, 1, 2, 3, 5, 8, 13, 21,
+
+// é importante dizer para a funcao deve parar.
+// fib (n – 1) + fib (n – 2)
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+	return fibonacci(posicao-1) + fibonacci(posicao-2)
+}
+
 func main() {
-	fmt.Println()
+	fmt.Println("Função Recursivas")
+
+	posicao := uint(5)
+
+	for i := uint(0); i < posicao; i++ {
+		fmt.Println(fibonacci(i))
+	}
+	fmt.Println("Na posição 6 está o nº:", posicao)
+
 }
