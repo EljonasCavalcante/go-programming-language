4db0eb26de4c091469b438b77c7840137de55382
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-06-19 21:13:33 -0300

    feat(STUD-10):create file test

---
+++ src/Teste Automatizados /introducao/enderecos/enderecos.go
@@ -0,0 +1,26 @@
+package enderecos
+
+import "strings"
+
+// TipoDeEndereco verifica se um endereco tem um tipo válido e o retorna
+func TipoDeEndereco(endereco string) string {
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+	enderecoEmLetraminuscula := strings.ToLower(endereco)
+
+	primeiraPalavraDoEndereco := strings.Split(enderecoEmLetraminuscula, " ")[0]
+
+	enderecoTemUmTipoValido := false
+
+	for _, tipo := range tiposValidos {
+		if tipo == primeiraPalavraDoEndereco {
+			enderecoTemUmTipoValido = true
+		}
+	}
+
+	if enderecoTemUmTipoValido {
+		return strings.Title(primeiraPalavraDoEndereco)
+	}
+
+	return "Tipo Inválido"
+
+}
---
+++ src/Teste Automatizados /introducao/introducaoTeste.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"introducao-testes/introducao/enderecos"
+)
+
+func main() {
+	tipoEndereco := enderecos.TipoDeEndereco("Rodovia dos Imigrantes")
+	fmt.Println(tipoEndereco)
+
+}
