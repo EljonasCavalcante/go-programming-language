6f8f1a97255cad925d045a0bc320b2aa49787dfe
EljonasCavalcante <eljonascavalcante@gmail.com>
2023-06-19 22:06:29 -0300

    update

---
+++ src/Modulo01/Teste avançado /formas/formas_test.go
@@ -0,0 +1,29 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Retângulo", func(t *testing.T) {
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A area recebida %f é diferente da esperada %f ", areaRecebida, areaEsperada)
+		}
+
+	})
+	t.Run("Círculo", func(t *testing.T) {
+		circ := Circulo{10}
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := circ.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A area recebida %f é diferente da esperada %f ", areaRecebida, areaEsperada)
+		}
+
+	})
+}
